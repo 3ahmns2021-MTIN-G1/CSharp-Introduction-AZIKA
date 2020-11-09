d8f70f84eb816b1e9f03059de50b3e3cfb5c604a
AnnaZika <annacarmen0@gmail.com>
2020-11-09 14:13:19 +0100

    Script in Unity, ExampleClass

---
+++ Assets/Script/ExampleClass.cs
@@ -0,0 +1,27 @@
+﻿
+//Bibliotheken /Asembly Referenz/Namenspace
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Deklaration einer Klasse
+
+//Public -> access modifier (Zugriffsrecht) | keyword vorreservierte Schlüssel
+//class -> keyword zur deklaration einer Klasse
+//ExampleClass -> identifier (name der klasse)
+// : MonoBehaviour -> erstellen einer unterklassse von mono behavior
+// {} -> Wirkungsbereich/Scope (Was ist alles in der klasse enthalten)
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
